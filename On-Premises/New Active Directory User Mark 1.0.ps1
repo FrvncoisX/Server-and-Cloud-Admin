19478ee6b009ca2b7a9422c018a4efedb618e690
FrvncoisX <99983148+FrvncoisX@users.noreply.github.com>
2022-03-14 13:08:50 -0400

    Init

---
+++ On-Premises/New Active Directory User Mark 1.0.ps1
@@ -0,0 +1,17 @@
+﻿#Script For New Active Directory User
+
+
+#Tittle
+Write-Host "New User Creator Mark 1.0"
+
+#Import the active directory module
+Import-Module ActiveDirectory
+
+#Data
+$firstname = Read-Host "First Name?"
+$lastname = Read-Host "Last name?"
+$username = "$firstname.$lastname"
+
+#Acctual Command
+
+New-aduser -name ("$firstname") -displayname ("$username") -Accountpassword (Read-Host -AsSecureString "AccountPassword") -Enabled $true
